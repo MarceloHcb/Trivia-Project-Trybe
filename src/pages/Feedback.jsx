94605b0b11475d2f7ed685d5b986bb75c7dacc59
Matheus Marinho <matheusmarinho57@gmail.com>
2023-02-13 16:38:05 -0300

    requisito 15 adiciona botão pra voltar a /

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -6,7 +6,7 @@ import Header from '../components/Header';
 class Feedback extends Component {
   render() {
     const correctAnwers = 3;
-    const { assertions, score } = this.props;
+    const { assertions, score, history } = this.props;
     return (
       <div data-testid="feedback-text">
         <Header />
@@ -19,18 +19,28 @@ class Feedback extends Component {
         <div data-testid="feedback-total-question">
           { assertions }
         </div>
+        <button
+          data-testid="btn-play-again"
+          onClick={ () => history.push('/') }
+        >
+          Play Again
+        </button>
       </div>
     );
   }
 }
-Feedback.propTypes = {
-  assertions: PropTypes.number.isRequired,
-  score: PropTypes.number.isRequired,
-};
 
 const mapStateToProps = (state) => ({
   assertions: state.player.assertions,
   score: state.player.score,
 });
+
+Feedback.propTypes = {
+  assertions: PropTypes.number.isRequired,
+  score: PropTypes.number.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default connect(mapStateToProps)(Feedback);
