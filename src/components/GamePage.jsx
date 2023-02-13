78dba61743ba66ebc046b6a75b68dcef5649a091
Marcelohcb <marcelohcb@msn.com>
2023-02-12 23:17:39 -0300

    refatora req 6, muda a cor da borda dos botoes entre verde e vermelho, req7

--- src/components/GamePage.jsx
+++ src/components/GamePage.jsx
@@ -4,60 +4,51 @@ import React, { Component } from 'react';
 class GamePage extends Component {
   state = {
     results: [],
-    answers: [],
     isLoading: true,
+    correct: [],
+    wrong: [],
+    border: '',
+    borderRed: '',
   };
 
   componentDidMount() {
     this.requestQuestions();
   }
 
-  createButtons = (data) => {
-    const magicNumber = 0.5;
-    const answers = (
-      <button
-        data-testid="correct-answer"
-        type="button"
-        key="5"
-      >
-        {data.results[0].correct_answer}
-
-      </button>);
-    const incAnswers = (
-      data.results[0].incorrect_answers.map((inc, index) => (
-        <button
-          data-testid={ `wrong-answer-${index}` }
-          type="button"
-          key={ index }
-        >
-          {inc}
-
-        </button>
-      ))
-    );
-
-    this.setState({
-      results: data.results,
-      answers: [...incAnswers, answers].sort(() => Math.random() - magicNumber),
-      isLoading: false,
-    });
-  };
-
   requestQuestions = async () => {
     const token = localStorage.getItem('token');
     const { history } = this.props;
     const response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
     const data = await response.json();
+    console.log(data);
+
     if (data.response_code !== 0) {
       localStorage.removeItem('token');
       history.push('/');
     } else {
-      this.createButtons(data);
+      this.setState({
+        results: data.results,
+        isLoading: false,
+        correct: data.results[0].correct_answer,
+        wrong: data.results[0].incorrect_answers.map((inc) => inc),
+      });
     }
   };
 
+  handleClick = () => {
+    this.setState({
+      borderRed: '3px solid red',
+      border: '3px solid rgb(6, 240, 15)',
+    });
+  };
+
   render() {
-    const { results, isLoading, answers } = this.state;
+    const magicNumber = 0.5;
+    const { results, isLoading, correct, wrong, border, borderRed } = this.state;
+    const answers = [...wrong, correct].sort(() => Math.random() - magicNumber);
+    console.log(answers);
+    console.log(wrong);
+    console.log(correct);
     return (
       <div>
         {isLoading === true ? <p>Loading...</p> : (
@@ -69,8 +60,36 @@ class GamePage extends Component {
               {results[0].question}
             </h2>
             <div data-testid="answer-options">
-              {answers}
+              { answers.map((answer, index) => (
+                answer === correct ? (
+                  <button
+                    key="8"
+                    type="button"
+                    value={ answer }
+                    data-testid="correct-answer"
+                    style={ { border } }
+                    onClick={ this.handleClick }
+                  >
+                    {' '}
+                    {answer}
+                  </button>
+                )
+                  : (
 
+                    <button
+                      data-testid={ `wrong-answer-${index}` }
+                      type="button"
+                      key={ index }
+                      value={ answer }
+                      style={ { border: borderRed } }
+                      className="green"
+                      onClick={ this.handleClick }
+                    >
+                      {answer}
+
+                    </button>
+                  )
+              ))}
             </div>
           </div>
         )}
