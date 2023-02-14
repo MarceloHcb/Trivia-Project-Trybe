bf0ade161e6688b7a0eb07d826c051b296b88ef7
leonardosantosb <leonardosantosbasilio@gmail.com>
2023-02-14 08:45:05 -0300

    req-18-refatorado

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -24,4 +24,3 @@ export const totalScore = (payload) => ({
 export const resetState = () => ({
   type: RESET_STATE,
 });
-
--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,4 +1,4 @@
-import { ADD_USER, UPDATE_SCORE, TOTAL_SCORE, RESET_STATE } from '../actions';
+import { ADD_USER, UPDATE_SCORE, RESET_STATE } from '../actions';
 
 const INITIAL_STATE = {
   name: '',
